28e04f5878280027188d1eea3bc49ddd9bad1b2e
Nitzan Cohen <nitzancohen@hotmail.com>
2023-05-18 22:22:15 +0300

    Added ts configuration and compiler

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var age = 9;
+var myName = "Nitzan";
+var circle = function (diameter) {
+    return diameter * Math.PI;
+};
+myName = "Rony";
+console.log(circle(13));
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+let age = 9;
+let myName = "Nitzan";
+
+const circle: (diameter: number) => number = (diameter: number) => {
+    return diameter * Math.PI;
+};
+
+myName = "Rony";
+console.log(circle(13));
