2485cf2eb4f7d1a4f1e7476b7ae92925fd3e7ae5
Nitzan Cohen <nitzancohen@hotmail.com>
2023-05-20 15:07:23 +0300

    Lesson: arraies and objects

--- sandbox.ts
+++ sandbox.ts
@@ -7,3 +7,74 @@ const circle: (diameter: number) => number = (diameter: number) => {
 
 myName = "Rony";
 console.log(circle(13));
+
+// ARRAIES
+let names = ["Nitzan", "Karin", "Rony"]
+// names = "Nitzan" // names is type of array, so it doesn't possible to replace it with a string.
+// names.push(3); // names is type of array, so it doesn't possible to push a number.
+names.push("Gal");
+
+// OBJECTS
+let person = {
+    name: "Nitzan",
+    age: 36
+}
+
+person.name = "Rony";
+person.age = 35;
+// person.age = "Thirty five" // The type of the value inside person.age is number, so it doesn't possible to replace it with a string.
+person.skills = ["Math", "Running", "Climbing"] // skills doesn't contain in the original object.
+
+person = {
+    name: "Gal",
+    age: 36,
+    // skills: ["Design", "Management"]
+}
+
+// EXPLICIT TYPES
+// Here I can define a variable and his type, without assign a value to it.
+let bookName: string;
+let pages: number;
+let isAvailable: boolean;
+
+// bookName = 30; // It doesn't possible, because the value needs to be as a string type.
+bookName = "Harry Potter";
+// pages = "Two hundreds fifty"; // it doesn't possible, because thevalue needs to be as a number type.
+pages = 250;
+// isAvailable = "true";
+isAvailable = true;
+
+let books: string[] = [];
+// books = "Harry Potter"; // books is an array type, not a string type.
+books.push("Harry Potter")
+
+// UNION TYPES
+let mixed: (number|string)[] = [];
+mixed.push("Nitzan");
+mixed.push(22);
+// mixed.push(false);
+
+let uid: string|number;
+uid = 12;
+uid = "ABC"
+// uid = true;
+
+// OBJECTS
+let personOne: object;
+personOne = {
+    name: "Karin",
+    age: 35,
+}; // It is possible, because {} is an object.
+ 
+personOne = [] // it is possible too, because [] is also an object.
+
+let personTwo: {
+    name: string,
+    age: number
+}
+
+personTwo = {
+    name: "Omer", 
+    age: 36, 
+    // hairColor: "Black" // This isn't appropriate to the personTwo definition.
+}
