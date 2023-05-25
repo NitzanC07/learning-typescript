9914b3be71b72f85d58e5db0527181ba11918d43
Nitzan Cohen <nitzancohen@hotmail.com>
2023-05-25 18:33:06 +0300

    Lesson 11: DOM interaction

--- public/app.js
+++ public/app.js
@@ -1,1 +1,5 @@
 "use strict";
+const element = document.querySelector("a");
+console.log(element === null || element === void 0 ? void 0 : element.href);
+const form = document.querySelector(".sample-form");
+console.log(form.children);
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+const element = document.querySelector("a");
+console.log(element?.href);
+
+const form = document.querySelector(".sample-form") as HTMLFormElement;
+console.log(form.children);
