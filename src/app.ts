5955c7dc9620b12c1dcc5e86a9b5c1df4c13b9e6
Nitzan Cohen <nitzancohen@hotmail.com>
2023-05-26 17:32:15 +0300

    Changed folders for adding new course

--- src/app.ts
+++
@@ -1,5 +0,0 @@
-const element = document.querySelector("a");
-console.log(element?.href);
-
-const form = document.querySelector(".sample-form") as HTMLFormElement;
-console.log(form.children);
---
+++ vanilla_typescript/public/app.js
@@ -0,0 +1,12 @@
+"use strict";
+/** Lesson: DOM & Type casting
+ *
+ */
+// כאשר אני בטוח שהרכיב שאני רוצה לבחור קיים בקוד המקור אני יכול להוסיף סימן קריאה בסוף השורה.
+const element1 = document.querySelector("a");
+console.log(element1.href);
+// במקרה בו אני לא בטוח שהרכיב קיים מומלץ להוסיף תנאי
+const element2 = document.querySelector("a");
+console.log(element2 === null || element2 === void 0 ? void 0 : element2.href);
+const form = document.querySelector(".sample-form");
+console.log(form.children);
