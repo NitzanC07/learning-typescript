6fa9c424d29c1063047bf0003636573c0f497852
Nitzan Cohen <nitzancohen@hotmail.com>
2023-05-29 14:51:29 +0300

    Lesson 11: DOM Elements

--- vanilla_typescript/src/app.ts
+++ vanilla_typescript/src/app.ts
@@ -10,8 +10,26 @@ console.log(element1.href);
 const element2 = document.querySelector("a");
 console.log(element2?.href);
 
+
 const form = document.querySelector(".sample-form") as HTMLFormElement;
 console.log(form.children);
 
-const input = document.querySelector(".input-fname") 
-console.log("Input", input);
+const fName = document.querySelector("#input-fname") as HTMLInputElement;
+console.log("Input", fName);
+
+const lName = document.querySelector("#input-lname") as HTMLInputElement;
+console.log("Input", lName);
+
+const myAge = document.querySelector("#input-age") as HTMLInputElement;
+console.log("Input", myAge);
+
+const btn = document.querySelector("#button-submit") as HTMLButtonElement;
+console.log("Button", btn);
+
+const output = document.querySelector(".form-result") as HTMLDivElement;
+console.log("Output", output);
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    output.textContent = `Hello ${fName.value} ${lName.value}, you're ${myAge.valueAsNumber} years old.`;
+})
